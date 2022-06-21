3c8657e9de515f82461775464859bc2982f927c8
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2022-06-21 10:55:02 -0300

    Utilizando o método print

---
+++ 02-print/print.py
@@ -0,0 +1,7 @@
+print("Ola")
+print("Ola", "Anne")
+print()
+print("Brasil ganhou 5 titulos mundiais")
+print("Brasil", "ganhou", "5", "titulos", "mundiais")
+print("Brasil", "ganhou", "5", "titulos", "mundiais", sep=" - ") #separador
+print("Brasil", "ganhou", "5", "titulos", "mundiais", end="\n") #pula linha
