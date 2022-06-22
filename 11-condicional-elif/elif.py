8686b56622e58c72b96e7a4ed7e1a6c1eb2731d0
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2022-06-22 14:49:25 -0300

    elif

---
+++ 11-condicional-elif/elif.py
@@ -0,0 +1,11 @@
+a = 0
+b = 1
+c = 1
+
+if a>b:
+    print("Numero maior: {}".format(a))
+elif b==c:
+    print("Sao iguais")
+else:
+    print("Numero maior: {}".format(b))
+print("Final do programa.")
