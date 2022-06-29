9796d81b6fe56c92dfd7abf854b10e3771143c0e
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2022-06-29 18:20:28 -0300

    Atividade 02 - Lista

---
+++ 00-lista-de-exercicios/01-estrutura-sequencial/02.py
@@ -0,0 +1,5 @@
+#02 - Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+#x = 2
+x = int(input("Informe um numero:"))
+print('O numero informado foi: {}'.format(x)) #utilizando format
+
