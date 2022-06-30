9f443ff10215c9374594cbc4b72c53b4c0fdc5a7
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2022-06-30 20:26:40 -0300

    Tuplas

---
+++ 17-tuplas/tuplas.py
@@ -0,0 +1,12 @@
+nomes = ('Ana', 'Bia', 'Gui', 'Ana')
+print(type(nomes)) #<class 'tuple'>
+print(nomes) #('Ana', 'Bia', 'Gui', 'Ana')
+
+print('Bia' in nomes) #TRUE
+
+#Acessando materiais
+print(nomes[0]) # Ana
+print(nomes[1:2]) # Bia
+print(nomes[1:-1]) # Bia e Gui
+
+
