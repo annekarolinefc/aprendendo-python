d7bbe10fdbf092b988a676fbe73a023942c14735
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2022-06-30 20:37:06 -0300

    Atualizando arquivos

--- 05-tipagem-python/exemplo01.py
+++ 05-tipagem-python/exemplo01.py
@@ -1,2 +1,10 @@
 preco = 49.99
 print(type(preco)) # <class 'float'>
+
+"""----------------------------"""
+
+print(type('Bem Vindo!'))
+print(type(1.1))
+print(type(1))
+print(type(False))
+print(type(True))
