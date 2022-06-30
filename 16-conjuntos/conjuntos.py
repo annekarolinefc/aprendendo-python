651724143882068e3ca7a9ac863aee3a3d61f06e
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2022-06-30 20:26:54 -0300

    Conjuntos

---
+++ 16-conjuntos/conjuntos.py
@@ -0,0 +1,7 @@
+print({1, 2, 3})
+print(type({1, 2, 3})) #<class 'set'>
+print({1, 2, 3, 3, 3, 3, 3}) #remove os repetidos
+
+
+conj = {1, 2, 3, 3, 3, 3, 3}
+#print(conj[1]) -> NÃO É ESTRUTURA INDEXADA
