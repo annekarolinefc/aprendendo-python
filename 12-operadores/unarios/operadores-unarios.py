f8db7f39c5f671077b59e533323a62a5eb0e5c09
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2022-06-30 20:27:13 -0300

    Operadores Unários

---
+++ 12-operadores/unarios/operadores-unarios.py
@@ -0,0 +1,15 @@
+"""Quantidade de operando
+    - not
+"""
+print(not False) #True
+print(not True) #False
+
+print(-3) # -3
+print(--3) # 3
+print(-y) # -y
+print(--y) # y
+print(+3)
+
+w = 12 
+# w++ -> NAO ESTA DISPONÍVEL
+print(w)
