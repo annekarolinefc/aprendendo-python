0c1657f16aaa25204e2131b76169bcb9c34a4a9d
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2022-07-04 19:00:52 -0300

    Concatenando String

---
+++ 06-manipulando-variaveis/concatena-string.py
@@ -0,0 +1,10 @@
+from typing import Annotated
+
+
+print("Ola, tuodo" + " bem?" + " Aqui ta" + " tudo bem.")
+
+nome = 'Anne' 
+sobrenome = 'Carmo' 
+
+nome_completo = nome + " " + sobrenome 
+print(nome_completo)
