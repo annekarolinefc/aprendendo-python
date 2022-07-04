9df83e35d388b4d51e4dd430382a9b8e95fcf8fb
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2022-07-04 20:16:23 -0300

    Pegando o horário atual

---
+++ 24-horario-atual/horario-atual-2.py
@@ -0,0 +1,5 @@
+# Como pegar o horario atual com Python?
+
+from datetime import datetime
+
+print(datetime.now())
---
+++ 24-horario-atual/horario-atual.py
@@ -0,0 +1,5 @@
+# Como pegar o horario atual com Python?
+
+import datetime
+
+print(datetime.datetime.now())
