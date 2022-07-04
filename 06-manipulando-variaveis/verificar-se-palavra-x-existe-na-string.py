2a272b38fce863265d852ba23f54583a5838c01f
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2022-07-04 19:13:33 -0300

    Verificar se palavra existe em uma frase: operador "not in"

---
+++ 06-manipulando-variaveis/verificar-se-palavra-x-existe-na-string.py
@@ -0,0 +1,10 @@
+# Verificar se string contém uma palavra em Python
+frase = 'O rato roeu a roupa do rei de Roma.'
+print(frase)
+
+palavra = 'rato'
+
+if palavra not in frase:
+    print("A frase não contem a palavra: " + palavra)
+else:
+    print("A frase contem a palavra: " + palavra)
