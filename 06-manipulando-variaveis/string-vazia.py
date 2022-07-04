0c37c84130ddcae2f9a7c7d076f9a889c3916ad9
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2022-07-04 19:00:32 -0300

    Verificar se uma string está vazia

---
+++ 06-manipulando-variaveis/string-vazia.py
@@ -0,0 +1,12 @@
+# Verificar se uma string esta vazia
+nome = 'Anne'
+string = ''
+
+if not nome:
+    print('String nome esta vazia.')
+    
+if not string:
+    print('String string esta vazia.')
+    
+if nome == "":
+    print('String nome esta vazia.')
