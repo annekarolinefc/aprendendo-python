b4733805d6ab7f66a5de367da555b39da5d890fc
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2022-07-04 18:14:00 -0300

    Operadores de Atribuição

---
+++ 12-operadores/atribuicao/operadores-de-atribuicao.py
@@ -0,0 +1,14 @@
+resultado = 2
+print(resultado)
+
+resultado = "texto"
+print(resultado)
+
+resultado = 2
+resultado += 2
+resultado -= 2
+resultado *= 2
+resultado /= 2
+resultado %= 2
+
+print(resultado)
