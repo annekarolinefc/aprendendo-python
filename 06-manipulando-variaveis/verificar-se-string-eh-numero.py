d6e2b34c8c0f4419aaf5b1e88b939be2a10ecbeb
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2022-07-04 19:32:42 -0300

    Verificar se uma string é um numero (somente inteiros)

---
+++ 06-manipulando-variaveis/verificar-se-string-eh-numero.py
@@ -0,0 +1,9 @@
+# Verificar se string é numero
+# isdigit()
+
+print("12".isdigit()) #TRUE
+print("teste".isdigit()) #FALSE
+print("123.78".isdigit()) #FALSE -> nao é validado. Somente para Inteiros.
+
+n = "13"
+print(n.isdigit()) #TRUE
