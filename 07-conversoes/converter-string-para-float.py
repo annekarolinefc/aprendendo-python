c7cd67bc4712b0bb3251040da8f155b3721495df
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2022-07-05 09:58:46 -0300

    Conversões | String para float e Float para Int

---
+++ 07-conversoes/converter-string-para-float.py
@@ -0,0 +1,15 @@
+y = '12.5'
+print(type(y)) #string
+
+#Transforma String em Float
+z = float(y)
+print(z + 5) #17.5
+print(type(z)) #float
+
+#Transforma float em int
+a = "30.3"
+b = float(a)
+print(type(b))
+c = int(b) #arredonda
+print(c)
+print(type(c))
