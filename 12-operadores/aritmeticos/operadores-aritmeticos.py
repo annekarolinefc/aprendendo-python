7f844b0140ffb5996dde6d9053220a5143e56fc5
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2022-07-08 14:44:24 -0300

    Operadores

--- 12-operadores/aritmeticos/operadores-aritmeticos.py
+++ 12-operadores/aritmeticos/operadores-aritmeticos.py
@@ -1,15 +1,22 @@
 x = 10
 y = 3
 
+#SOMA
 print(x+y)
+#SUBTRAÇÃP
 print(x-y)
+#MULTIPLICAÇÃO
 print(x*y)
+#DIVISÃO
 print(x/y)
+#RESTO DE UMA DIVISAO
 print(x%y)
+#POTENCIA
+print(x**y)
+
 
 par = 34
 impar = 33
-
 #Detectar se um numero é par ou impar
 print(par % 2==0)
 print(impar % 2==1)
--- 12-operadores/relacionais/operadores-relacionais.py
+++ 12-operadores/relacionais/operadores-relacionais.py
@@ -1,10 +1,13 @@
 x = 7
 y = 5
 
-print(x > y)
-print(x >= y)
-print(x < y)
-print(x <= y)
-print(x == y)
-print(x != y)
+print(x > y) #MAIOR QUE
+print(x >= y) # MAIOR OU IGUAL QUE
+print(x < y) #MENOR QUE
+print(x <= y) #MENOR OU IGUAL QUE
+print(x == y) #IGUAL
+print(x != y) # NÃO IGUAL
 print('5' != 5) #true
+
+#AND
+#OR
