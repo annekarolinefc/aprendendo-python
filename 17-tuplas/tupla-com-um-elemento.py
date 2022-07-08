ecfc6120c9e544300c2244412950773f5654da56
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2022-07-07 21:47:43 -0300

    Tuplas

---
+++ 17-tuplas/tupla-com-um-elemento.py
@@ -0,0 +1,6 @@
+tupla1 = 'a',
+tupla2 = ('b',)
+
+#colocar virgula
+print(tupla1)
+print(tupla2)
---
+++ 17-tuplas/tuplas_dias_da_semana.py
@@ -0,0 +1,2 @@
+dias_da_semana = ('Domingo', 'Segunda', 'Terca', 'Quarta', 'Quinta', 'Sexta', 'Sabado')
+print(type(dias_da_semana))
