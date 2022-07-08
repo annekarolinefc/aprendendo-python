b49fa461c2666cef11f58c0120ee0b0be7706838
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2022-07-07 21:47:28 -0300

    Listas

---
+++ 14-listas/contar-elementos-lista.py
@@ -0,0 +1,3 @@
+funcionarios = ['Astrid','Flavia','Talia','Mauricio', 'Waldemar', 'Marina']
+print(funcionarios)
+print("Tamanho da lista é: {}".format(len(funcionarios)))
---
+++ 14-listas/lista_carros.py
@@ -0,0 +1,12 @@
+#LISTAS TIPO ARRYA
+carros = ['Camaro','Lamborghini', 'Ferrari']
+
+#OPERACOES COM ARRAYS
+carros.append('Hilux') #adiciona um objeto na ultima posição do Array
+carros.insert(0, 'Hilux') #Insere um objeto em uma posição específica do Array
+
+#REMOVE UM OBJETO NO ARRAY PELO NOME
+carros.remove('Hilux')
+
+#REMOVE UM OBJETO NO ARRAY PELA POSIÇÃO
+carros.pop(3)
